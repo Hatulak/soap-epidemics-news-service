968a54d79aa3895c6a1f6d911a9c9b3f8c92d991
Mateusz Pernal <exomat@wp.pl>
2020-05-01 17:34:04 +0200

    Add logging handler for all request and response.

--- src/main/java/com/hatexomatulak/snes/configs/Config.java
+++ src/main/java/com/hatexomatulak/snes/configs/Config.java
@@ -1,18 +1,27 @@
 package com.hatexomatulak.snes.configs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.ws.client.WebServiceClientException;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.XsdSchemaCollection;
 import org.springframework.xml.xsd.commons.CommonsXsdSchemaCollection;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
 @EnableWs
 @Configuration
 @EnableAsync
@@ -23,6 +32,69 @@ public class Config extends WsConfigurerAdapter {
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean(servlet, "/service/*");
+    }
+
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(new EndpointInterceptor() {
+            @Override
+            public boolean handleRequest(MessageContext messageContext, Object o) throws Exception {
+                log.info("[SOAP RESPONSE]");
+                try {
+                    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                    messageContext.getResponse().writeTo(buffer);
+                    String payload = buffer.toString(java.nio.charset.StandardCharsets.UTF_8.name());
+                    log.info(payload);
+                } catch (IOException e) {
+                    throw new WebServiceClientException("Can not write the SOAP response into the out stream", e) {
+                        private static final long serialVersionUID = -7118480620416458069L;
+                    };
+                }
+
+                return true;
+            }
+
+            @Override
+            public boolean handleResponse(MessageContext messageContext, Object o) throws Exception {
+                log.info("[SOAP REQUEST]");
+                try {
+                    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                    messageContext.getRequest().writeTo(buffer);
+                    String payload = buffer.toString(java.nio.charset.StandardCharsets.UTF_8.name());
+                    log.info(payload);
+                } catch (IOException e) {
+                    throw new WebServiceClientException("Can not write the SOAP request into the out stream", e) {
+                        private static final long serialVersionUID = -7118480620416458069L;
+                    };
+                }
+
+                return true;
+            }
+
+            @Override
+            public boolean handleFault(MessageContext messageContext, Object o) throws Exception {
+
+                log.info("[SOAP REQUEST]");
+                try {
+                    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                    messageContext.getRequest().writeTo(buffer);
+                    String payload = buffer.toString(java.nio.charset.StandardCharsets.UTF_8.name());
+                    log.info(payload);
+                } catch (IOException e) {
+                    throw new WebServiceClientException("Can not write the SOAP request into the out stream", e) {
+                        private static final long serialVersionUID = -7118480620416458069L;
+                    };
+                }
+
+                return true;
+            }
+
+            @Override
+            public void afterCompletion(MessageContext messageContext, Object o, Exception e) throws Exception {
+
+            }
+        });
     }
 
     @Bean(name = "news")
